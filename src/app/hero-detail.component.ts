9496483ae9f0b8028f120fdc01b459b592c8c9d8
Jacky <jacky@Jacky-MBP.lan>
2017-08-07 18:16:54 +0800

    some codes

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -8,17 +8,7 @@ import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} detail</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-      <button (click)="goBack()">Back</button>
-    </div>
-  `
+  templateUrl: './hero-detail.component.html'
 })
 
 export class HeroDetailComponent implements OnInit {
